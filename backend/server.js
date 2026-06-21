be9986f1770c76f081434d07327a333f5476dbec
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-06-22 01:18:09 +0600

    Write: CORS to registering our routers

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookies from "cookie-parser";
+import cors from "cors";
+
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+   origin: process.env.CLIENT_URL|| "http://localhost:5173",
+   credentials: true,
+ })
+);
+
 app.use(express.json());
 app.use(cookies());
+
+
 
 const PORT = process.env.PORT || 5000;
 
