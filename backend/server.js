2ee9a9096e200ecd3b58e589f0717affa09b0488
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-06-21 22:47:54 +0600

    Update: server.js file Create: auth.js file in tha routes folder

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookies from "cookie-parser";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cookies());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
