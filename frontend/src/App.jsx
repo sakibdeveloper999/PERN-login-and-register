6c5923313f1d25ad727b1fd6f7c02f3f3639635b
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-06-22 14:54:15 +0600

    initial setup for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import {useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
+
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true);
+
+
   return (
-    <div>PERN Auth App</div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
