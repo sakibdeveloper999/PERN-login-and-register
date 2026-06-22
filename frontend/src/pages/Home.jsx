2849faea20f4dea71107f4cdd549db1a24a47ac2
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-06-23 04:02:11 +0600

    update: looking good in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
-const Home = () => {
+const Home = ({ user }) => {
+  
   return (
-    <div>Home</div>
-  )
-}
+    <div className='max-w-md mx-auto mt-10 p-6 border border-gray-300 rounded-md'>
+      <h1 className='text-3xl font-bold mb-3'>
+        Welcome, {user?.name}
+      </h1>
+      <p className='text-gray-600'>
+        You are successfully logged in.
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
