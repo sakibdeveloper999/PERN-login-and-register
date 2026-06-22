6e91b50f1a87035c054dbdeccfd6125485442d1c
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-06-22 15:31:36 +0600

    Write:  Fetch user profile on app load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,40 @@
-import {useState } from 'react'
+import {useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 const App = () => {
 
   const [user, setUser] = useState(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
+
+  // Fetch user profile on app load
+  useEffect(()=>{
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("localhost:5000/api/auth/profile")
+        setUser(res.data);
+      } catch (err) {
+        setError("Failed to fetch user");
+        setUser(null);
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
 
   return (
